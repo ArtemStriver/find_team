202b3adf9a821116862f1f510b5f87f36f01a64d
Artem Striver <dog-di@mail.ru>
2023-11-19 08:51:49 +0500

    Сделал панель навигации, первое представление главной страницы и заглушку страницы с информацией.

--- find_team/main/views.py
+++ find_team/main/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    data = {
+        'title': 'Про нас',
+    }
+    return render(request, 'main/about.html', data)
