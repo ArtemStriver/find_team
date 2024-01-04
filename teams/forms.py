6ee21ad834f49cf220647ca6fd1d0647ae3433e9
Artem Striver <dog-di@mail.ru>
2024-01-04 17:35:57 +0500

    Реорганизовал файловую структуру, подготовил проект к контейнеризации через docker.

---
+++ teams/forms.py
@@ -0,0 +1,50 @@
+import datetime
+
+from .models import Teams, JoinInTeam
+from django.forms import ModelForm, TextInput, Textarea, DateInput, DateTimeField
+
+
+class TeamsForm(ModelForm):
+    """Форма для добавления команды."""
+    deadline = DateTimeField(
+        required=False,
+        widget=DateInput(
+            attrs={'type': 'datetime-local',
+                   'class': 'form-control',
+                   'placeholder': "Крайний срок для сбора команды"}
+        ),
+        initial=datetime.date.today(),
+        localize=True
+    )
+
+    class Meta:
+        model = Teams
+        fields = ['owner', 'title', 'intro', 'text', 'tags']
+
+        widgets = {
+            'title': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': "Заголовок для команды"
+            }),
+            'intro': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': "Краткое описание команды"
+            }),
+            'text': Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': "Подробное описание команды"
+            }),
+            'tags': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': "Теги для быстрого поиска"
+            })
+        }
+
+
+class JoinForm(ModelForm):
+    """Форма для заявки в команду."""
+    text = Textarea()
+
+    class Meta:
+        model = JoinInTeam
+        fields = ['title', 'author', 'text', 'team_boss']
